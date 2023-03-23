1892dc9fbf9f692cdfa3f622c1d8c96400f1f486
Assia Benkhelifa <99099827+Assiabk@users.noreply.github.com>
2023-03-23 11:59:03 -1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Ask the user for their name
+const name=prompt("What is your name,pls");
+// Create a personalized greeting
+const greeting = `Hello, ${name}! Welcome to our website!`;
+// Display the greeting on the screen
+alert(greeting);
